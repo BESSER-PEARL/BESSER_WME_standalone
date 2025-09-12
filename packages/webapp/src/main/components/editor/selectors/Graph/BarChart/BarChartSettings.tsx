78f2ad6183841aba5dfa29dfffa214f5ce63d7ef
ivan-alfonso <ivandaalf@hotmail.com>
2025-09-12 14:42:27 +0200

    Fix import paths in BarChartSettings and reset default x/y positions in BarChart component

--- packages/webapp/src/main/components/editor/selectors/Graph/BarChart/BarChartSettings.tsx
+++ packages/webapp/src/main/components/editor/selectors/Graph/BarChart/BarChartSettings.tsx
@@ -1,5 +1,5 @@
-import { ToolbarItem } from '../../../Toolbar/ToolbarItem';
-import { ToolbarSection } from '../../../Toolbar/ToolbarSection';
+import { ToolbarItem } from '../../../editor-bars/ToolbarItem';
+import { ToolbarSection } from '../../../editor-bars/ToolbarSection';
 import { DimensionsSettings, MarginSettings, PositionSettings } from '../../CommonSettings';
 
 export const BarChartSettings = () => {
